c8af098430f9dbb9b88b7473d2f532bd586c5c25
Manuel Michelangelli <mmichelangelli@moveapps.cl>
2021-06-16 09:19:08 -0400

    api platzi store

---
+++ src/recap.ts
@@ -0,0 +1,24 @@
+const myName = 'Nicolas';
+const myAge = 12;
+const suma = (a: number, b: number) => {
+  return a + b;
+};
+
+suma(12, 23);
+
+class Persona {
+/*   private age;
+  private name; */
+
+  constructor(private age: number, private name: string) {
+/*     this.age = age
+    this.name = name */
+  }
+
+  getSummary() {
+    return `my name is ${this.name}, ${this.age}`;
+  }
+}
+
+const nicolas = new Persona(15, 'nicolas')
+nicolas.getSummary()
